d17086a2fb516616c322c3b728e4bc7ff82f8902
alex <aharrystone@gmail.com>
2024-10-06 14:29:24 -0500

    added train spirit to page

--- src/SpiritStats.jsx
+++ src/SpiritStats.jsx
@@ -6,6 +6,28 @@ function SpiritStats(){
     const [scared, setScared] = useState(5);
     const [tired, setTired] = useState(5);
 
+
+    return (
+        <div>
+            <h3>Spirit Stats</h3>
+            <table>
+                <thead>
+                <tr>
+                    <td>hungry</td>
+                    <td>scared</td>
+                    <td>tired</td>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>{hungry}</td>
+                    <td>{scared}</td>
+                    <td>{tired}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
     
 }
 
