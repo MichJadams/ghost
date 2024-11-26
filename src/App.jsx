f89372154362fbab7a520b7072669ed32cecca38
Michjadams <mjadams66@gmail.com>
2024-11-25 21:25:47 -0500

    breaks everything, but soon it will be built up again!

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SubwayMap from './subwayMap/SubwayMap'
 import SelectedStation from './SelectedStation'
 import SpiritStats from './SpiritStats'
 import Spirit from './Spirit'
+import SubwayMapFirstPerson from "./SubwayMapFirstPerson/SubwayMapFirstPerson.jsx";
 import './index.css'
 
 
@@ -16,8 +17,9 @@ function App() {
   return (
     <>
       <h1>Metrogatchi</h1>
-      <SubwayMap />
-      <SelectedStation stationName={'125th St'} stationComplexName={'125 St (4,5,6)'} />
+        <SubwayMapFirstPerson />
+      {/*<SubwayMap />*/}
+      {/*<SelectedStation stationName={'125th St'} stationComplexName={'125 St (4,5,6)'} />*/}
     </>
   )
 }
